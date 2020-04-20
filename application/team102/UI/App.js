cbca492f4ceb7e771bfa5386cd7d1a445465b248
awong27 <45473248+awong27@users.noreply.github.com>
2020-04-20 12:59:22 -0700

    Added more routes

--- application/team102/UI/App.js
+++ application/team102/UI/App.js
@@ -1,36 +1,50 @@
-import React, {useState} from 'react';
-import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
-import Navi from "./Component/Navigation.js";
+import React, {useState, Component} from 'react';
+import { Switch, Route, BrowserRouter} from "react-router-dom";
+//import Navi from "./Component/Navigation.js";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./Component/About.js";
+import SList from "./Component/ShoppingList.js";
 import inventory from "./Component/inventory.js";
+import mealplan from "./Component/MealPlan.js";
 import Home from "./Component/Home.js";
-//import Register from "./Register.js";
-import statistics from "./Component/Statistics.js";
+import Register from "./Register.js";
+import Login from "./Login.js";
+import Welcome from "./Welcome.js";
+import Statistics from "./Component/Statistics.js";
+import StatWeekly from "./Component/StatWeekly.js";
+//import Nav2 from "./components/navbar.component";
+//import Inventory from "./components/inventory.component";
+//import EditFoodData from "./components/edit-fooddata.component";
 import CreateFood from "./Component/create-food.component.js";
 
 function App() {
-  
-  return (
-    <div>
-    <BrowserRouter>
-    <Navi/>
-    <div className="App">
-        <Switch>
-          <Route path="/Home" exact component={Home} />
-          <Route path="/About" exact component={About} />
-          <Route path="/inventory" exact component={inventory} />
-          <Route path="/mealplan" exact component={mealplan} />
-          <Route path="/statistics" exact component={statistics} />
-          <Route path="/create" exact component={CreateFood}/>
-        </Switch>
-      </div>    
-      </BrowserRouter>
+  /** */   
+    //const[visibility,inVis] = useState(false);
+    
+    return (
+      <div align="center">          
+      <BrowserRouter>       
+        <div className="Main">
+          <Switch>
+            <Route path="/" exact component={Welcome} />
+            <Route path="/Register" exact component={Register} />
+            <Route path="/Login" exact component={Login} />
+            <Route path="/Home" exact component={Home} />
+            <Route path="/About" exact component={About} />
+            <Route path="/SList" exact component={SList} />
+            <Route path="/inventory" exact component={inventory} />
+            <Route path="/mealplan" exact component={mealplan} />
+            <Route path="/statistics" exact component={Statistics} />  
+            <Route path="/StatWeekly" exact component={StatWeekly} />        
+            <Route path="/create" exact component={CreateFood}/>
+          </Switch>
+        </div>    
+      </BrowserRouter> 
     </div>
   );
 }
-
-
-const mealplan = () => <h1>Meal Plan<h2>Search for balanced meals</h2></h1>;
+//<Nav2/>
+//<Route path="/" exact component={Inventory}/>
+//<Route path="/edit/:id" exact component={EditFoodData}/>
 export default App;
