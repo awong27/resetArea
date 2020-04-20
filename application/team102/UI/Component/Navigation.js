912b442b20832ed508412e7ccf528e1734ad5c79
awong27 <45473248+awong27@users.noreply.github.com>
2020-04-20 12:44:01 -0700

    Updated Nav with Icons

--- application/team102/UI/Component/Navigation.js
+++ application/team102/UI/Component/Navigation.js
@@ -9,49 +9,61 @@ import {
   NavLink,
   Navbar
 } from "reactstrap";
-//import './Nav.css';
+import './Nav.css';
+
+import navHome from './NavHome.png'
+import navCart from './NavCart.png'
+import navFridge from './NavFridge.png'
+import navMeal from './NavMeal.png'
+import navPerson from './NavPerson.png'
 
 const Navigation = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-
+  const [openTab,setTab] = useState(0);
   const toggle = () => setDropdownOpen(!dropdownOpen);
-
+  
   return (
-    <Nav >
-      <Navbar fixed='bottom'> 
-      
-        <NavItem>
-          <NavLink href="/Home"  active>
-            <button>Home</button>
-          </NavLink>
-        </NavItem>
-        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle nav caret>
-            <button>Meal Plan</button>
-          </DropdownToggle>
-          <DropdownMenu>            
-            <DropdownItem>Meal Plan</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>Recipes</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <NavItem>
-          <NavLink href="/create" ><button>Create</button></NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/About" ><button>About</button></NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/inventory" ><button>Inventory</button></NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink  href="/statistics" >
-          <button>Statistics</button>
-          </NavLink>
-        </NavItem>
      
-      </Navbar> </Nav>
-    
+    <Nav onScroll='fixed' fill='true'>
+      <Navbar  fixed='bottom' color="light" className="navbar-light navbar-expand-sm" > 
+         
+      <NavItem onClick={()=> setTab(0)} >       
+        <NavLink href="/Home" active={openTab===0} >  
+          <img alt="Home" src ={navHome}/>           
+        </NavLink>
+        </NavItem>  
+      
+      <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle nav  active={openTab===1}>
+          <img alt="MealPlan" src ={navMeal}/>            
+        </DropdownToggle>
+        <DropdownMenu>            
+          <DropdownItem><NavLink href="/mealplan">Meal Plan</NavLink></DropdownItem>
+          
+          <DropdownItem><NavLink href="/mealplan">Recipes</NavLink></DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      
+      <NavItem onClick={()=> setTab(2)}>
+        <NavLink href="/SList" active={openTab===2}>
+          <img alt="ShoppingList" src ={navCart}/>
+        </NavLink>
+      </NavItem>
+            
+      <NavItem onClick={()=> setTab(3)}>
+        <NavLink href="/inventory" active={openTab===3}>
+          <img alt="inv" src ={navFridge}/>
+        </NavLink>
+      </NavItem>
+      
+      <NavItem onClick={()=> setTab(4)}> 
+        <NavLink  href="/statistics" active={openTab===4}>
+          <img alt="Stats" src ={navPerson}/>
+        </NavLink>
+      </NavItem>   
+      
+      </Navbar> 
+    </Nav> 
   );
 };
 
