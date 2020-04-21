affe3ad8230b182fa3b66495e64f4a2e281bb07e
awong27 <45473248+awong27@users.noreply.github.com>
2020-04-20 18:56:15 -0700

    added topbar

--- application/team102/UI/src/Component/ShoppingList.js
+++ application/team102/UI/src/Component/ShoppingList.js
@@ -1,32 +1,34 @@
 import React, {useState, Component} from 'react';
 import {Table, Button, Row, Col, ButtonGroup} from 'reactstrap';
 import Navi from "./Navigation";
+import TopBar from "./TopBar"; 
 
 export default class ShoppingList extends Component {
     
   
   render() {      
     return (
-      <div>
-      <div><h1>Shopping List</h1></div>      
+      <div><TopBar/><br/><br/>
+      <div><h1>Shopping List</h1></div>   
+      <div className="listItem">   
       <Button className="invBar"><Row>
-        <Col/><Col className="itemName">Banana</Col><Col className="expire">3/15</Col><Col/><Col/><Col/>
+        <Col className="itemName">Banana</Col><Col className="expire">3/15</Col><Col/><Col/><Col/>
       </Row></Button>
       <Button className="invBar"><Row>
-        <Col/><Col className="itemName">Beef Shank</Col><Col className="expire">4/8</Col><Col/><Col/><Col/>
+        <Col className="itemName">Beef Shank</Col><Col className="expire">4/8</Col><Col/><Col/><Col/>
       </Row></Button>
       <Button className="invBar"><Row>
-        <Col/><Col className="itemName">Kiwi</Col><Col className="expire">9/1</Col><Col/><Col/><Col/>
+        <Col className="itemName">Kiwi</Col><Col className="expire">9/1</Col><Col/><Col/><Col/>
       </Row></Button>
       <Button className="invBar"><Row>
-        <Col/><Col className="itemName">Bread</Col><Col className="expire">2/10</Col><Col/><Col/><Col/>
+        <Col className="itemName">Bread</Col><Col className="expire">2/10</Col><Col/><Col/><Col/>
       </Row></Button>
       <Button className="invBar"><Row>
-        <Col/> <Col className="itemName">Tomato</Col><Col className="expire">6/24</Col><Col/><Col/><Col/>
-      </Row></Button>
+        <Col className="itemName">Tomato</Col><Col className="expire">6/24</Col><Col/><Col/><Col/>
+      </Row></Button></div>
       <ButtonGroup size='lg'>
         <Button href="/create">History</Button>
-        <Button href="/create">Scan</Button>
+        <Button href="/create">Add</Button>
         <Button href="/inventory">Add to Inventory</Button>
       </ButtonGroup>
       <Navi/>
