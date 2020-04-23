5b0946bad56f77e70c826aae8deb96f675fcf45e
Dgodfrey78 <godfrey2012danny@gmail.com>
2020-04-23 15:43:34 -0700

    Add files via upload
    meal plan model

---
+++ application/team102/backend/models/mealplan.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const mealplanSchema = new Schema(
+  {
+    date:{type:String},
+    restrictions:[{type:String}],
+    planCalories: { type: String},
+    planCarbs: {type:String},
+    planSugar:{type:String},
+    planFat:{type:String},
+    planProtein:{type:String},
+    creator: {type:String}
+  }
+
+);
+
+const Mealplandata = mongoose.model("Mealplandata", mealplanSchema);
+
+module.exports = Mealplandata;
