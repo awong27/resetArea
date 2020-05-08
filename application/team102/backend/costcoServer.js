31c68b96e4c6e84a628edcbaa2c8fb3159e93ef8
dpolozov <dpolozov@gmail.com>
2020-05-07 19:59:49 -0700

    vission sends array to costco gets descriptions of all items

--- application/team102/backend/costcoServer.js
+++ application/team102/backend/costcoServer.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const port = 3005;
+const port = 3006;
 
 const app = express();
 
-rp('https://www.costcobusinessdelivery.com/1312297')
-    .then((html) => {
-        const $ = cheerio.load(html);
-        const info = $('.description').text();
-        console.log(info);
-    })
-    .catch((e) => {
-        console.log(e);
+const bodyParser = require('body-parser');
+app.use(bodyParser());
+
+app.post('/api/description/get', (req, res) => {
+
+    console.log(req.body);
+    const itemsIds = req.body.itemsIds;
+
+    itemsIds.forEach(item => {
+    
+        rp(`https://www.costcobusinessdelivery.com/${item}`)
+        .then((html) => {
+            const $ = cheerio.load(html);
+            const info = $('.description').text();
+            console.log(info);
+        })
+        .catch((e) => {
+            //console.log(e);
+            //add to array item not found.
+        });
+        
     });
+
+});
 
    
 app.listen(port);
--- application/team102/backend/visionServer.js
+++ application/team102/backend/visionServer.js
@@ -3,6 +3,8 @@ const Vision = require('@google-cloud/vision');
 const vision = new Vision.ImageAnnotatorClient({
     keyFilename: './visionAssets/visionKey.json'
 });
+const axios = require('axios');
+
 
 const app = express();
 
@@ -12,9 +14,21 @@ vision
     .textDetection('./visionAssets/receipts/1.jpg')
     .then(results => {
         const texts = results[0].textAnnotations;
+        const itemsIds = [];
         
         console.log('text:');
-        texts.forEach(text => console.log(text.description));
+        texts.forEach(text => {
+            let id = Number.parseInt(text.description);
+            if (id > 9999 && id < 1000000) {
+                itemsIds.push(id);
+            }
+        });
+
+        axios.post('http://localhost:3006/api/description/get', {
+            itemsIds
+        });
+        
+        //console.log(itemsId);
     })
     .catch(err => {
         console.log(err);
