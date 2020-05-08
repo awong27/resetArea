c7c35f982ca854218c65923bcb16d0b499cf2260
Matthew <stonegolem98@gmail.com>
2020-05-08 10:10:01 -0700

    Added Logout Button
    Added a button to the edit user page that redirects the user to the login page.

--- application/team102/UI/src/Component/EditUser.js
+++ application/team102/UI/src/Component/EditUser.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import Tphoto from "./thanos.png";
-import { Container, Row, Col, NavLink} from "reactstrap";
+import { Container, Row, Col, NavLink, Button} from "reactstrap";
 import Navi from "./Navigation";
-import TopBar from "./TopBar"; 
+import TopBar from "./TopBar";
 import "./Home.css"
 
 export default class EditUser extends Component{
-    
-  render() { 
+
+  render() {
     return (
       <div> <TopBar/>
-      <Container className="HomePage">       
-        <Row><Col> 
+      <Container className="HomePage">
+        <Row><Col>
           <NavLink href="/About"><img alt="UserEdit" src={Tphoto} className="userEdit"/></NavLink>
         </Col><Col>
             <br/><br/><br/>
@@ -19,25 +19,30 @@ export default class EditUser extends Component{
             <NavLink><p>Edit Name</p></NavLink>
             <NavLink><p>Settings</p></NavLink>
         </Col>
-        </Row>  
-              
+        </Row>
+
         <Row>
           <Col><NavLink href="/mealplan"><img alt="MealPlan" src={Tphoto} className="iconsUser"/> </NavLink> </Col>
           <Col><NavLink><p>Spouse</p></NavLink></Col>
           <Col><NavLink><p>Edit</p></NavLink></Col>
-        </Row>  
+        </Row>
         <Row>
           <Col><NavLink href="/Statistics"><img alt="Stats" src={Tphoto} className="iconsUser"/></NavLink> </Col>
           <Col><NavLink><p>Child 1</p></NavLink></Col>
           <Col><NavLink><p>Edit</p></NavLink></Col>
-        </Row>  
+        </Row>
         <Row>
           <Col><NavLink href="/inventory"><img alt="Inv" src={Tphoto} className="iconsUser"/></NavLink></Col>
           <Col><NavLink><p>Child 2</p></NavLink></Col>
           <Col><NavLink><p>Edit</p></NavLink></Col>
-        </Row>             
-      </Container>      
-      <Navi/> 
+        </Row>
+        <Row>
+        <Col>
+          <Button href="/" color="danger" >Sign Out</Button>
+        </Col>
+        </Row>
+      </Container>
+      <Navi/>
       </div>
     );
   }
