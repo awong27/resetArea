107c3c6c36e8f5a77c831e86ed0be62245c737a5
Dgodfrey78 <godfrey2012danny@gmail.com>
2020-05-08 16:52:31 -0700

    Update Home.js

--- application/team102/UI/src/Component/Home.js
+++ application/team102/UI/src/Component/Home.js
@@ -1,6 +1,6 @@
 import React, {Link, Component} from 'react';
 import Tphoto from "./thanos.png";
-import { Container, Row, Col, NavLink, ListGroup, ListGroupItem, Badge} from "reactstrap";
+import { Container, Row, Col, NavLink} from "reactstrap";
 import Navi from "./Navigation";
 import TopBar from "./TopBar";
 import Fphoto from "./fridgeIcomn.png";
@@ -16,12 +16,14 @@ export default class Home extends Component{
   constructor(props) {
   super(props);
 
+  const {match:{params}} = this.props;
+
   this.deleteItems = this.deleteItems.bind(this);
 
   this.state = {
     userdata: [],
-    password:"",
-    username:""
+    password:params.password,
+    username:params.id
 
 
    };
@@ -36,15 +38,13 @@ componentDidMount() {
     .get(`http://localhost:8080/userdata/${params.id}`)
     .then(response => {
       this.setState({ userdata: response.data });
+      console.log(this.state.userdata)
 
     })
     .catch(error => {
       console.log(error);
     });
-    this.setState({
-      password: this.state.userdata.password,
-      username: this.state.userdata.username
-    })
+
 }
 
 
@@ -53,65 +53,43 @@ deleteItems(id) {
     .delete("http://localhost:8080/userdata/" + id)
     .then(res => console.log(res.data));
   this.setState({
-    userdata: this.state.userdata.filter(el => el._id !== id)
+    userdata: this.state.userdata.filter(el => el._id != id)
   });
 }
 
   render() {
-    console.log(this.state.userdata);
+    const {match:{params}} = this.props;
+    console.log(this.state.username);
+    console.log(this.state.password);
 
+
+
+    var inv= "/inventory/"+params.id+"/"+params.password;
+    var stat= "/Statistics/"+params.id+"/"+params.password;
+    var meal="/mealplan/"+params.id+"/"+params.password;
+    //console.log(user);
 
     return (
       <div align = "center" > <TopBar/>
       <Container className="HomePage">  <br/>
 
-        <Row className="homeRow">
-            <Col className="homeSquare">
-            <h3>Notifications</h3>
-            <ListGroup overflow="hidden">
-              <ListGroupItem className="justify-content-between">2 Weeks <Badge pill>14</Badge></ListGroupItem>
-              <ListGroupItem className="justify-content-between">Expires Soon <Badge pill>2</Badge></ListGroupItem>
-              <ListGroupItem className="justify-content-between">Expired <Badge pill>1</Badge></ListGroupItem>
-            </ListGroup>
-            </Col>
-            <Col className="homeSquare">
-              
-            
-            </Col>          
-        </Row>
+        <Row><Col>
+          <NavLink href="/EditUser"><img alt="UserPic" src={Tphoto} className="userPic"/></NavLink>
+        </Col></Row>
 
-        <Row className="homeRow">
-          <Col className="homeSquare">
-            <NavLink href="/mealplan">
-              <h1>MealPlan</h1>
-              <h3>Chicken Quesadilla</h3>
-              <h3>Duck</h3>
-              <h3>Rice and Beans</h3>
-            </NavLink>
-          </Col>
-          <Col className="homeSquare">
-            <NavLink href="/Recipes">
-              <h1>Recent Recipes</h1>
-              <h3>Tiramisu</h3>
-              <h3>PotPies</h3>
-              <h3>Omelete</h3>
-            </NavLink>
-          </Col>          
+        <Row>
+          <Col><NavLink href={meal}><img alt="MealPlan" src={Mphoto} className="icons"/> </NavLink> </Col>
+          <Col><NavLink href={stat}><img alt="Stats" src={Dphoto} className="icons"/></NavLink> </Col>
+          <Col><NavLink href={inv}><img alt="Inv" src={Fphoto} className="icons"/></NavLink></Col>
         </Row>
-        
+        <Row>
+          <Col><p>Meal Plan</p> </Col>
+          <Col><p>Statistics</p> </Col>
+          <Col><p>Inventory</p> </Col>
+        </Row>
       </Container>
-      <Navi/>
+      <Navi username={this.state.username} password={this.state.password} />
       </div>
     );
   }
 }
-/**
- * <NavLink href="/Statistics"><img alt="Stats" src={Dphoto} className="icons"/></NavLink>
- * <NavLink href="/mealplan"><img alt="MealPlan" src={Mphoto} className="icons"/> </NavLink>
- * <Row className="homeRow">
-          <Col className="homeSquare"><NavLink href="/inventory"><img alt="Inv" src={Fphoto} className="icons"/></NavLink></Col>
-          <Col className="homeSquare"><NavLink href="/inventory"><img alt="Inv" src={Fphoto} className="icons"/></NavLink></Col>
-        </Row>
- * 
- * 
- */
