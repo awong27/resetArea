1f5dc62b7cf74917ae0dec0b4f60e6b8ba35bde7
Dgodfrey78 <godfrey2012danny@gmail.com>
2020-05-08 16:53:34 -0700

    Update ShoppingList.js

--- application/team102/UI/src/Component/ShoppingList.js
+++ application/team102/UI/src/Component/ShoppingList.js
@@ -32,7 +32,130 @@ export default class ShoppingList extends Component {
       <Button className="invBar"><Row>
         <Col className="itemName">Tomato</Col><Col className="expire">6/24</Col><Col/><Col/>
       </Row></Button></div>
-      <ButtonGroup size='lg' >
+      <Buttoimport React, { Component} from 'react';
+import {Table, Button, Row, Col, ButtonGroup} from 'reactstrap';
+import Navi from "./Navigation";
+import TopBar from "./TopBar";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Fooddata = props => (
+  <div>
+  <Button className="invBar"><Row>
+<Col className="itemName">{props.food.itemName}</Col><Col className="expire">3/15</Col><Col className="quantity">{props.food.itemAmount}</Col><Col className="actions"></Col>
+</Row></Button>
+  <tr>
+    <td>{props.food.itemName}</td>
+    <td>{props.food.itemAmount}</td>
+
+    <td>
+      <Link to={"/edit/" + props.food._id}>edit</Link> |{" "}
+      <a
+        href="/create"
+        onClick={() => {
+          props.deleteItems(props.food._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+  </div>
+);
+
+export default class ShoppingList extends Component {
+
+  constructor(props) {
+    super(props);
+    const {match:{params}} = this.props;
+    this.deleteItems = this.deleteItems.bind(this);
+
+    this.state = {
+      userdata: [],
+      shoppingList: [],
+      username: params.id,
+      password: params.password
+
+     };
+  }
+
+  componentDidMount() {
+    const {match:{params}}= this.props;
+
+    axios
+      .get("http://localhost:8080/userdata/")
+      .then(response => {
+        this.setState({ userdata: response.data });
+        this.state.userdata.map(currentfood=>{
+          if(currentfood.username==params.id){
+            console.log(currentfood.username);
+            this.setState({shoppingList:currentfood.shoppingList});
+            console.log(currentfood.shoppingList);
+          }
+        })
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  deleteItems(id) {
+    axios
+      .delete("http://localhost:8080/userdata/" + id)
+      .then(res => console.log(res.data));
+    this.setState({
+      userdata: this.state.userdata.filter(el => el._id !== id)
+    });
+  }
+
+  shoppinglist() {
+
+
+    const {match:{params}} = this.props;
+    return this.state.shoppingList.map(currentfood => {
+
+        return (
+
+          <Fooddata
+            food={currentfood}
+            deleteItems={this.deleteItems}
+            key={currentfood._id}
+            />
+          );
+    });
+  }
+
+  render() {
+    return (
+      <div><TopBar/>
+      <br/><br/><br/>
+      <div><h1>Shopping List</h1></div>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Exp. Date</th>
+            <th>Quantity</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.shoppinglist()}
+        </tbody>
+      </Table>
+
+      <ButtonGroup size='lg'>
+        <Button href="/create">Delete</Button>
+        <Button href="/create">Scan</Button>
+        <Button href="/create">Eat</Button>
+      </ButtonGroup>
+      <Navi username={this.state.username} password={this.state.password} />
+      </div>
+    )
+  }
+}
+nGroup size='lg' >
         <Button href="/SHist">History</Button>
         <Button href="/create">New Item</Button>
         <Button href="/inventory">+ Inventory</Button>
