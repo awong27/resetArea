0a1b3214680db9d2b09368566a11db91d81694bf
awong27 <awong27@mail.sfsu.edu>
2020-05-08 22:32:39 -0700

    fixed home button on topnav

--- application/team102/UI/src/Component/TopBar.js
+++ application/team102/UI/src/Component/TopBar.js
@@ -6,19 +6,23 @@ import {
   Navbar
 } from "reactstrap";
 import './Nav.css';
-
+import NavHome from "./Home.svg";
 import Tphoto from "./thanos.png";
 
 const TopBar = () => {
-    
+  
   return (
      
     <Nav onScroll='fixed' fill='true' className='d-block'>
-      <Navbar  fixed='top' color="light"  > 
-               
-      <NavItem className="logo" ><h1>DIRT</h1></NavItem>
-      
-      <NavItem/><NavLink href="/Home/:id/:password"> home</NavLink><NavItem/>
+      <Navbar  fixed='top' color="light" href="Home/:id/:password" > 
+      <NavItem>
+        <NavLink href="/Home/:id/:password" >
+          <img alt="Home" src ={NavHome} className="nav-pic"/> 
+        </NavLink>
+      </NavItem> 
+      <NavItem/><NavItem/> 
+      <NavItem ><h1>DIRT</h1></NavItem>
+      <NavItem/><NavItem/>
       <NavItem> 
         <NavLink href="/EditUser"><img alt="UserPic" src={Tphoto} className="userPic"/></NavLink>
       </NavItem>   
