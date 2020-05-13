2667c605ddd94228c52405d21c242d3d4370c730
Dgodfrey78 <godfrey2012danny@gmail.com>
2020-05-13 13:52:12 -0700

    Update App.js
    Changed routes to send around credentials. Will probably be updated but this navigates for now

--- application/team102/UI/src/App.js
+++ application/team102/UI/src/App.js
@@ -1,49 +1,70 @@
 import React from "react";
 import { Switch, Route, BrowserRouter} from "react-router-dom";
+//import Navi from "./Component/Navigation.js";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./Component/About.js";
 import SList from "./Component/ShoppingList.js";
 import SHist from "./Component/ShoppingHistory.js";
-import inventory from "./Component/inventory.js";
+import Inventory from "./Component/inventory.js";
 import mealplan from "./Component/MealPlan.js";
+import mealDay from "./Component/MealDay.js";
 import inRep from "./Component/RecipeInner.js"
 import Recipe from "./Component/Recipe.js"
 import Home from "./Component/Home.js";
 import EditUser from "./Component/EditUser.js"
-import SignIn from "./SignIn.js";
+import Register from "./Register.js";
+import Login from "./Login.js";
+import Welcome from "./Welcome.js";
 import Statistics from "./Component/Statistics.js";
 import StatWeekly from "./Component/StatWeekly.js";
+import SearchRecipe from "./Component/addRecipe.js";
+//import Nav2 from "./components/navbar.component";
+//import Inventory from "./components/inventory.component";
+//import EditFoodData from "./components/edit-fooddata.component";
 import CreateFood from "./Component/create-fooddata.component.js";
+import FoodCard from "./Component/user-inventory.component.js";
 
 function App() {
+  /** */
+    //const[visibility,inVis] = useState(false);
+
     return (
       <div align="center">
       <BrowserRouter>
         <div className="Main">
           <Switch>
-            <Route path="/" exact component={SignIn} />
+            <Route path="/" exact component={Welcome} />
+            <Route path="/Register" exact component={Register} />
+            <Route path="/Login" exact component={Login} />
 
             <Route path="/Home/:id/:password" exact component={Home} />
             <Route path="/EditUser" exact component={EditUser} />
             <Route path="/About" exact component={About} />
 
             <Route path="/SList/:id/:password" exact component={SList} />
-            <Route path="/SHist/:id/:password" exact component={SHist} />
-            <Route path="/inventory/:id/:password" exact component={inventory} />
-
+            <Route path="/SHist" exact component={SHist} />
+            <Route path="/inventory/:id/:password" exact component={Inventory} />
+            <Route path="/user-item/:id" component={FoodCard}/>
             <Route path="/mealplan/:id/:password" exact component={mealplan} />
-            <Route path="/Recipe/:id/:password" exact component={Recipe} />
-            <Route path="/inRep" exact component={inRep} />
+            <Route path="/mealDay" exact component={mealDay} />
 
-            <Route path="/statistics" exact component={Statistics} />
+            <Route path="/Recipe/:id/:password" exact component={Recipe} />
+            <Route path="/addRecipe/:id/:password/:search/:sugar/:fat/:carbs/:calories" exact component={SearchRecipe} />
+            <Route path="/inRep/:id/:password/:recipeName" exact component={inRep} />
+
+            <Route path="/Statistics/:id/:password" exact component={Statistics} />
             <Route path="/StatWeekly" exact component={StatWeekly} />
-            <Route path="/create" exact component={CreateFood}/>
+            <Route path="/create/:id/:password" exact component={CreateFood}/>
+
+
           </Switch>
         </div>
       </BrowserRouter>
     </div>
   );
 }
-
+//<Nav2/>
+//<Route path="/" exact component={Inventory}/>
+//<Route path="/edit/:id" exact component={EditFoodData}/>
 export default App;
