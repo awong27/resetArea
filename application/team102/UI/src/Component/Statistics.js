7cd6b996466d658120a19fb3cbddae6ff57c66c4
Dgodfrey78 <godfrey2012danny@gmail.com>
2020-05-13 16:46:40 -0700

    Update Statistics.js
    nav update

--- application/team102/UI/src/Component/Statistics.js
+++ application/team102/UI/src/Component/Statistics.js
@@ -1,11 +1,11 @@
 import React, { Component} from 'react';
 import {Progress,Container, Row, Col, Table, Button, ButtonGroup} from 'reactstrap';
 import Navi from "./Navigation";
-import TopBar from "./TopBar"; 
+import TopBar from "./TopBar";
 
 export default class Statistics extends Component {
   render() {
-    return (       
+    return (
       <div>   <TopBar/><br/><br/><br/>
       <Container>
         <Row><Col><h1>Statistics</h1></Col></Row>
@@ -17,16 +17,16 @@ export default class Statistics extends Component {
             <Progress bar color="info" value="25" />
             <Progress bar color="warning" value="20" />
             <Progress bar color="danger" value="5" />
-            </Progress> 
+            </Progress>
         </Col> <Col xs="1"></Col>
-        </Row>        
-          
+        </Row>
+
         <Row><Col xs="1"></Col>
-        <Col><Table striped>      
+        <Col><Table striped>
       <tbody>
         <tr>
           <th scope="row">Calories</th>
-          <td></td> <td>57,352</td>          
+          <td></td> <td>57,352</td>
         </tr>
         <tr>
           <th scope="row">Fats</th>
@@ -34,28 +34,27 @@ export default class Statistics extends Component {
         </tr>
         <tr>
           <th scope="row">Sodium</th>
-          <td></td> <td>179 g</td>          
+          <td></td> <td>179 g</td>
         </tr>
         <tr>
           <th scope="row">Protien</th>
-          <td></td> <td>1,423 g</td>          
+          <td></td> <td>1,423 g</td>
         </tr>
         <tr>
           <th scope="row">Fluids</th>
-          <td></td> <td>15 gal</td>          
+          <td></td> <td>15 gal</td>
         </tr>
         </tbody>
         </Table></Col>
-        </Row>   
+        </Row>
         <ButtonGroup size='lg'>
           <Button href="/StatWeekly">Monthly</Button>
           <Button href="/Statistics">Daily</Button>
           <Button href="/StatWeekly">Weekly</Button>
-        </ButtonGroup>                  
-      </Container>    
-      <Navi/>     
-      </div>   
+        </ButtonGroup>
+      </Container>
+      <Navi username={this.state.username} password={this.state.password} />
+      </div>
     );
   }
 }
-  
