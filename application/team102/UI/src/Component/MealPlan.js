b0883f0f150760878ceb85db3342ac0dd8b83b1f
awong27 <awong27@mail.sfsu.edu>
2020-05-15 16:49:51 -0700

    add buttons to mealplan

--- application/team102/UI/src/Component/MealPlan.js
+++ application/team102/UI/src/Component/MealPlan.js
@@ -98,14 +98,17 @@ export default class MealPlan extends Component {
     * summates total calories at the top
     * should keep track of each day and time
     * data should be sent to userdata for stats
+    * 
+    * need to find a way to add recipies to it either by id or tag
+    * need to store days by calendar id to pull by day
+    * refactor by week to show prev week's meals
+    * should also load which day it is and load that as active
     */
     return (     
       <div className="container"><TopBar username={this.state.username} password={this.state.password}/>        
       <Container>  <br/><br/><br/>
       <Row><Col><h1>Meal Plan</h1></Col></Row>
-      
- 
-      <h3>Total Calories: 3,431</h3>
+
       <Nav tabs justified className="plan">
         <NavItem>
           <NavLink className={classnames({ active: this.state.activeTab === '1' })}
@@ -153,37 +156,39 @@ export default class MealPlan extends Component {
       <TabContent activeTab={this.state.activeTab}>
         <TabPane tabId="1">
           <Row>
-            <Col></Col><Col></Col><Col><p>Calories</p></Col>
+            <Col></Col><Col><p>Calories</p></Col><Col></Col>
           </Row>
           <Row>
-            <Col><h3>Breakfast</h3></Col><Col><Col></Col></Col>
+            <Col><h3>Breakfast</h3></Col><Col></Col><Col></Col>
           </Row>
           <Row>
-            <Col><p>Cereal</p></Col><Col></Col><Col>235</Col>
+            <Col><p>Cereal</p></Col><Col></Col><Col>235</Col><Button>X</Button><Col xs='1'/>
           </Row>
           <Row>
-            <Col><p>Milk</p></Col><Col></Col><Col>54</Col>
+            <Col><p>Milk</p></Col><Col></Col><Col>54</Col><Button>X</Button><Col xs='1'/>
           </Row>
           <Row>
             <Col><h3>Lunch</h3></Col><Col><Col></Col></Col>
           </Row>
           <Row>
-            <Col><p>Chicken Pot Pie</p></Col><Col></Col><Col>900</Col>
+            <Col><p>Chicken Pot Pie</p></Col><Col>900</Col><Col></Col><Button>X</Button>
           </Row>
           <Row>
-            <Col><p>Soda</p></Col><Col></Col><Col>120</Col>
+            <Col><p>Soda</p></Col><Col>120</Col><Col></Col><Button>X</Button>
           </Row>
           <Row>
             <Col><h3>Dinner</h3></Col><Col><Col></Col></Col>
           </Row>
           <Row>
-            <Col><p>Corn Hen</p></Col><Col></Col><Col>608</Col>
+            <Col><p>Corn Hen</p></Col><Col>608</Col><Col><Button>X</Button></Col>
           </Row>
           <Row>
-            <Col><p>Mashed Potatoes</p></Col><Col></Col><Col>348</Col>
+            <Col><p>Mashed Potatoes</p></Col><Col>348</Col><Col><Button>X</Button></Col>
           </Row>
+          <Row className="bottom"></Row>
         </TabPane>
         <TabPane tabId="2">
+          <h3>Total Calories: 3,431</h3>
           <Row/>
           <Row><h3>Breakfast</h3></Row>
           <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
@@ -197,19 +202,74 @@ export default class MealPlan extends Component {
           <Row className="bottom"></Row>
         </TabPane>
         <TabPane tabId="3">
-          this is tuesday
+        <h3>Total Calories: 0</h3>
+          <Row/>
+          <Row><h3>Breakfast</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Lunch</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Dinner</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>          
+          <Row className="bottom"></Row>
         </TabPane>
         <TabPane tabId="4">
-          this is Wednesday
+        <h3>Total Calories: 0</h3>
+          <Row/>
+          <Row><h3>Breakfast</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Lunch</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Dinner</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>          
+          <Row className="bottom"></Row>
         </TabPane>
         <TabPane tabId="5">
-          this is Thursday
+        <h3>Total Calories: 0</h3>
+          <Row/>
+          <Row><h3>Breakfast</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Lunch</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Dinner</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>          
+          <Row className="bottom"></Row>
         </TabPane>
         <TabPane tabId="6">
-          this is Friday
+        <h3>Total Calories: 0</h3>
+          <Row/>
+          <Row><h3>Breakfast</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Lunch</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Dinner</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>          
+          <Row className="bottom"></Row>
         </TabPane>
         <TabPane tabId="7">
-          this is Saturday
+        <h3>Total Calories: 0</h3>
+          <Row/>
+          <Row><h3>Breakfast</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Lunch</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>
+          <Row><h3>Dinner</h3></Row>
+          <Row><Col>- - -</Col><Col/><Button>Add Item</Button></Row>
+          <Row/>          
+          <Row className="bottom"></Row>
         </TabPane>
       </TabContent>
       </Container>
