76fc4c1ccff9a609000ca960dad0a1d0dcadd2d5
awong27 <awong27@mail.sfsu.edu>
2020-05-19 05:17:58 -0700

    merged recipe and addrep and split recipedata into new file

--- application/team102/UI/src/Component/Recipe.js
+++ application/team102/UI/src/Component/Recipe.js
@@ -7,53 +7,8 @@ import {
 import Navi from "./Navigation";
 import TopBar from "./TopBar";
 import axios from "axios";
+import RecipeData from "./RecipeData.js"
 
-//grabs recipe information
-//displays as a rectangular picture with overlay text
-//when clicked with have a pop up with more info
-const Fooddata = props => {
-  const { className } = props;
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
-  return (
-    <Card className="bigdes" body inverse='true'>
-      <CardImg alt="recipeItem" src={props.food.recipeImage} />
-      <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}><h3>{props.food.recipeName}</h3></ModalHeader>
-        <ModalBody>
-          <h4>Calories:{props.food.recipeCalories}</h4>
-          <h4>Carbs:{props.food.recipeCarbs}</h4>
-          <h4>Sugar:{props.food.recipeSugar}</h4>
-          <h4>Protein:{props.food.recipeProtein}</h4>
-          <h4>Fat:{props.food.recipeFat}</h4>
-          <img alt={props.food.recipeName} src={props.food.recipeImage} height="50%" width="100%" />
-        </ModalBody>
-        <ModalFooter>
-          <ButtonGroup className="itemOptbtn">
-            <Button onClick={() => { props.deleteItems(props.food._id); }}>
-
-            </Button>
-            <Button>
-              <CardText>Add to Meal Plan</CardText>
-            </Button>
-            <Button onClick={() => "/create" + props.food._id}>
-
-            </Button>
-          </ButtonGroup>
-        </ModalFooter>
-      </Modal>
-      <CardImgOverlay onClick={toggle} style={{ backgroundColor: "rgba(0, 0, 0, 0.375)" }}>
-        <CardTitle position='fixed'><h4>{props.food.recipeName}</h4></CardTitle>
-        <CardText>Calories: {props.food.recipeCalories > 0 ? Math.round(props.food.recipeCalories) : 0}</CardText>
-      </CardImgOverlay>
-    </Card>
-  );
-}
-//grabs search results
-const Recipedata = props => (
-  <tr>{props.food.recipe.label}</tr>
-);
 
 export default class Recipe extends Component {
 
@@ -66,10 +21,10 @@ export default class Recipe extends Component {
     this.onChangeSugar = this.onChangeSugar.bind(this);
     this.onChangeSodium = this.onChangeSodium.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-
     this.deleteItems = this.deleteItems.bind(this);
+    this.searchRepClean = this.searchRepClean.bind(this);
+    this.sort = this.sort.bind(this);
     const { match: { params } } = this.props;
-
     this.state = {
       creator: params.id,
       password: params.password,
@@ -78,12 +33,12 @@ export default class Recipe extends Component {
       newrecipedata: [],
       newRecipes: [],
       recipename: "",
-      recipeCalories: "NA",
-      recipeCarbs: "NA",
-      recipeFat: "NA",
-      recipeProtein: "NA",
-      recipeSugar: "NA",
-      recipeSodium: "NA",
+      recipeCalories: "",
+      recipeCarbs: "",
+      recipeFat: "",
+      recipeProtein: "",
+      recipeSugar: "",
+      recipeSodium: "",
       addedRecipe: "",
       ingredients: [],
       recipeImage: "",
@@ -93,115 +48,70 @@ export default class Recipe extends Component {
       sugar: params.sugar
     };
   }
-  onChangeSearch(e) {
-    this.setState({
-      searches: e.target.value
-    });
-  }
-  onChangeCalories(e) {
-    this.setState({
-      recipeCalories: e.target.value.replace(/\D/g, '')
-    });
-    console.log(this.state.recipeCalories)
-  }
-  onChangeCarbs(e) {
-    this.setState({
-      recipeCarbs: e.target.value.replace(/\D/g, '')
-    });
-  }
-  onChangeSugar(e) {
-    this.setState({
-      recipeSugar: e.target.value.replace(/\D/g, '')
-    });
-  }
-  onChangeSodium(e) {
-    this.setState({
-      recipeSodium: e.target.value.replace(/\D/g, '')
-    });
-  }
-  onChangeProtein(e) {
-    this.setState({
-      protein: e.target.value
-    });
-  }
-  onChangeFat(e) {
-    this.setState({
-      recipeFat: e.target.value.replace(/\D/g, '')
-    });
-  }
+  searchRepClean(){this.setState({ newrecipedata:[]});}
+  onChangeSearch(e) { this.setState({ searches: e.target.value }); }
+  onChangeCalories(e) { this.setState({ recipeCalories: e.target.value.replace(/\D/g, '') }); }
+  onChangeCarbs(e) { this.setState({ recipeCarbs: e.target.value.replace(/\D/g, '') }); }
+  onChangeSugar(e) { this.setState({ recipeSugar: e.target.value.replace(/\D/g, '') }); }
+  onChangeSodium(e) { this.setState({ recipeSodium: e.target.value.replace(/\D/g, '') }); }
+  onChangeProtein(e) { this.setState({ protein: e.target.value }); }
+  onChangeFat(e) { this.setState({ recipeFat: e.target.value.replace(/\D/g, '') }); }
+  /**onSubmit sends a search request to edamam api with current params */
   onSubmit(e) {
     e.preventDefault();
     axios
-      .get("https://api.edamam.com/search?q=chicken&app_id=b53160ee&app_key=5d9984e95c5c6968d5edfb7d02c83b46&from=0&to=3&calories=591-722&health=alcohol-free")
+      .get(`https://api.edamam.com/search?q=${this.state.searches}&app_id=b53160ee&app_key=5d9984e95c5c6968d5edfb7d02c83b46&from=0&to=10&calories=0-${this.state.recipeCalories}${this.state.recipeFat}${this.state.recipeCarbs}${this.state.recipeSugar}${this.state.recipeSodium}`)
       .then(response => {
-        let newrec = [];
-        var label;
-        //console.log(response.data);
-        this.setState({
-          newrecipedata: response.data.hits
-        });
-        this.state.newrecipedata.map(currentfood => {
-          console.log(currentfood.recipe.label);
-          label = currentfood.recipe.label;
-          console.log(label);
-          newrec.push(label);
-          console.log(newrec);
-          currentfood.recipe.ingredients.map(currentingredient => {
-            console.log(currentingredient.text);
-            return (null);
-          })
-          return (null);
-
-        })
-        console.log(newrec);
-        this.setState({
-          newRecipes: newrec
-        })
-        console.log(this.state.newRecipes);
+        this.setState({ newrecipedata: response.data.hits });
+        this.sort();
       })
-    var addRec = "/addRecipe/" + this.state.creator + "/" + this.state.password + "/" + this.state.searches + "/" + this.state.recipeSugar + "/" + this.state.recipeFat + "/" + this.state.recipeCarbs + "/" + this.state.recipeCalories
-    window.location = addRec
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  /** sort will take the new recipe data and only grab what is needed
+   * and creates a new list of recipes for search results
+   * cleans up results data
+   */
+  sort() {
+    let newList = [];
+    this.state.newrecipedata.map(currentfood => {
+      let newR = {
+        ingredients: [],
+        recipeName: "",
+        recipeImage: "",
+        recipeCalories: "",
+        recipeProtein: "",
+        recipeFat: "",
+        recipeCarbs: "",
+        recipeSugar: "",
+        recipeSodium: "",
+        creator: this.state.creator,
+      };
+      newR.ingredients= currentfood.recipe.ingredientLines;
+      newR.recipeName = currentfood.recipe.label;
+      newR.recipeImage = currentfood.recipe.image;
+      newR.recipeCalories = currentfood.recipe.calories;
+      newR.recipeProtein = currentfood.recipe.totalNutrients.PROCNT.quantity; 
+      newR.recipeFat = currentfood.recipe.totalNutrients.FAT.quantity;
+      newR.recipeCarbs = currentfood.recipe.totalNutrients.CHOCDF.quantity;
+      newR.recipeSugar = currentfood.recipe.totalNutrients.SUGAR.quantity;
+      newR.recipeSodium = currentfood.recipe.totalNutrients.NA.quantity;    
+      console.log(newR);
+      newList.push(newR);
+      return (null);
+    })    
+    this.setState({
+      newRecipes: newList
+    })
+    console.log(this.state.newRecipes);
+    this.searchRepClean();
   }
   componentDidMount() {
     axios
       .get("http://localhost:8080/recipedata/")
       .then(response => {
         this.setState({ recipedata: response.data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  componentDidUpdate(props) {
-    const { match: { params } } = this.props;
-    //console.log({itemid});
-    var search = encodeURI(params.search);
-    var calories = "";
-    if (params.calories !== "NA") {
-      calories = encodeURI(`${params.calories}`);
-    }
-    var fat = "";
-    if (params.fat !== "NA") {
-      fat = encodeURI(`&nutrients[FAT]=${params.fat}`);
-    }
-    var sugar = "";
-    if (params.sugar !== "NA") {
-      sugar = encodeURI(`&nutrients[SUGAR]=${params.sugar}`);
-    }
-    var carbs = "";
-    if (params.carbs !== "NA") {
-      carbs = encodeURI(`&nutrients[CHOCDF]=${params.carbs}`);
-    }
-    axios
-      .get(`https://api.edamam.com/search?q=${search}&app_id=b53160ee&app_key=5d9984e95c5c6968d5edfb7d02c83b46&from=0&to=10&calories=0-${calories}${fat}${carbs}${sugar}`)
-      .then(response => {
-        //let newrec = [];
-        this.setState({ recipedata: response.data.hits });
-        console.log(this.state.recipedata);
-        this.state.recipedata.map(currentfood => {
-          console.log(currentfood.recipe.label);
-          return(null);
-        })
       })
       .catch(error => {
         console.log(error);
@@ -215,47 +125,11 @@ export default class Recipe extends Component {
       recipedata: this.state.recipedata.filter(el => el._id !== id)
     });
   }
-  addRecipe(name, recipedata, user) {
-    console.log(recipedata);
-    var ingred = "";
-    const newR = {
-      ingredients: [],
-      recipeName: "",
-      recipeImage: "",
-      recipeCalories: "",
-      recipeProtein: "",
-      recipeFat: "",
-      recipeCarbs: "",
-      recipeSugar: "",
-      creator: user,
-    };
-    //const {match:{params}} = this.props;
-
-    newR.recipeName = recipedata.recipe.label;
-    newR.recipeProtein = recipedata.recipe.totalNutrients.PROCNT.quantity;
-    newR.recipeImage = recipedata.recipe.image;
-    newR.recipeCalories = recipedata.recipe.calories;
-    newR.recipeFat = recipedata.recipe.totalNutrients.FAT.quantity;
-    newR.recipeCarbs = recipedata.recipe.totalNutrients.CHOCDF.quantity;
-    newR.recipeSugar = recipedata.recipe.totalNutrients.SUGAR.quantity;
-
-    recipedata.recipe.ingredients.map(currentingredient => {
-      ingred = currentingredient.text;
-      newR.ingredients.push(ingred);
-      console.log(newR.ingredients);
-      return(null);
-    })
-    /*    
-                  const newR = {
-                recipeName : this.state.recipename,
-                ingredients : this.state.newrec,
-              }  
-               console.log(newR);
-    */
-    console.log(newR);
+  addRecipe(newR) {    
     axios
       .post("http://localhost:8080/recipedata/add", newR)
       .then(res => console.log(res.data));
+    this.onChangeSearch("");
   };
 
   inventory() {
@@ -263,11 +137,10 @@ export default class Recipe extends Component {
     return this.state.recipedata.map(currentfood => {
       if (currentfood.creator === params.id) {
         return (
-          <Fooddata
+          <RecipeData
             food={currentfood}
             username={params.id}
             password={params.password}
-            deleteItems={this.deleteItems}
             key={currentfood._id}
           />
         );
@@ -277,12 +150,10 @@ export default class Recipe extends Component {
 
   searchedRecipes() {
     //const { match: { params } } = this.props;
-    return this.state.newrecipedata.map(currentfood => {
+    return this.state.newRecipes.map(currentfood => {
       return (
-        <Recipedata
-          recipedata={this.state.recipedata}
+        <RecipeData
           food={currentfood}
-          deleteItems={this.deleteItems}
           addRecipe={this.addRecipe}
           key={currentfood._id}
         />
@@ -408,7 +279,7 @@ export default class Recipe extends Component {
         <Container className="fit-content">
           <Row><Col><h1>Recipes</h1></Col></Row>
           {this.restrictions()}
-          {this.inventory()}
+          {this.state.searches !== "" ? this.searchedRecipes() : this.inventory()}
           <br />
         </Container>
         <Navi username={this.state.creator} password={this.state.password} />
@@ -416,3 +287,39 @@ export default class Recipe extends Component {
     )
   }
 }
+
+/**var search = encodeURI(params.search);
+    var calories = "";
+    if (params.calories !== "NA") {
+      calories = encodeURI(`${params.calories}`);
+    }
+    var fat = "";
+    if (params.fat !== "NA") {
+      fat = encodeURI(`&nutrients[FAT]=${params.fat}`);
+    }
+    var sugar = "";
+    if (params.sugar !== "NA") {
+      sugar = encodeURI(`&nutrients[SUGAR]=${params.sugar}`);
+    }
+    var carbs = "";
+    if (params.carbs !== "NA") {
+      carbs = encodeURI(`&nutrients[CHOCDF]=${params.carbs}`);
+    }
+
+
+
+    let ingredients = [];
+          currentfood.recipe.ingredients.map(currentingredient => {
+            console.log(currentingredient.text);
+            ingredients.push(currentingredient.text);
+            return (null);
+          })
+
+
+          recipedata.recipe.ingredients.map(currentingredient => {
+      ingred = currentingredient.text;
+      newR.ingredients.push(ingred);
+      console.log(newR.ingredients);
+      return (null);
+    })
+     */
---
+++ application/team102/UI/src/Component/RecipeData.js
@@ -0,0 +1,57 @@
+import React, { useState, Component } from 'react';
+import {
+    Container, Row, Col, Input, Button, ButtonGroup,
+    Card, CardImg, Form, CardText, CardTitle, CardImgOverlay,
+    FormGroup, Modal, ModalHeader, ModalBody, ModalFooter,
+} from 'reactstrap';
+
+//grabs recipe information
+//displays as a rectangular picture with overlay text
+//when clicked with have a pop up with more info
+const RecipeData = props => {
+    const { className } = props;
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+    return (
+        <Card className="bigdes" body inverse={true}>
+            <CardImg alt="recipeItem" src={props.food.recipeImage} />
+            <Modal isOpen={modal} toggle={toggle} className={className}>
+                <ModalHeader toggle={toggle}><h3>{props.food.recipeName}</h3></ModalHeader>
+                <ModalBody>
+                    <Row>
+                        <Col>
+                            <h5>Calories: {Math.round(props.food.recipeCalories)}</h5>
+                            <h5>Carbs: {Math.round(props.food.recipeCarbs)} g</h5>
+                            <h5>Protein: {Math.round(props.food.recipeProtein)} g</h5>
+                            <h5>Fat: {Math.round(props.food.recipeFat)} g</h5>
+                            <h5>Sugar: {Math.round(props.food.recipeSugar)} g</h5>
+                            <h5>Sodium: {Math.round(props.food.recipeSodium)} mg</h5>
+                        </Col>
+                        <img alt={props.food.recipeName} src={props.food.recipeImage} height="35%" width="50%" style={{ borderRadius: "30px" }} />
+                    </Row>                    
+                    <Row style={{boxShadow: "0px -2px rgba(36, 177, 153, 0.514)", height: "7.5px"}}/>
+                    <h3>Ingredients</h3>
+                    {props.food.ingredients.map(item => { return (<h5>{item}</h5>); })}
+                </ModalBody>
+                <ModalFooter>
+                    <ButtonGroup className="itemOptbtn">
+                        <Button >
+
+                        </Button>
+                        <Button>
+                            <CardText>Add to Meal Plan</CardText>
+                        </Button>
+                        <Button onClick={() => "/create" + props.food._id}>
+
+                        </Button>
+                    </ButtonGroup>
+                </ModalFooter>
+            </Modal>
+            <CardImgOverlay onClick={toggle} style={{ backgroundColor: "rgba(0, 0, 0, 0.375)" }}>
+                <CardTitle position='fixed'><h4>{props.food.recipeName}</h4></CardTitle>
+                <CardText>Calories: {props.food.recipeCalories > 0 ? Math.round(props.food.recipeCalories) : 0}</CardText>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+export default RecipeData;
