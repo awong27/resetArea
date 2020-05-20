6768aa8510a6c91df52e711dd70cd54ae7218813
Matthew <stonegolem98@gmail.com>
2020-05-19 22:45:41 -0700

    Content Update
    Made it possible to add and remove members from family and  preferences.

--- application/team102/UI/src/Component/EditUser.js
+++ application/team102/UI/src/Component/EditUser.js
@@ -1,5 +1,7 @@
 import React, {Component, useState } from 'react';
-import Tphoto from "./thanos.png";
+import Tphoto from "./blankProfile.png";
+import TopBar from "./TopBar";
+import Bphoto from "./blankProfile.png"
 
 import { Container, Row, Col, NavLink, Button, ListGroup, ListGroupItem,
         Modal, ModalHeader, ModalBody, ModalFooter,
@@ -7,53 +9,85 @@ import { Container, Row, Col, NavLink, Button, ListGroup, ListGroupItem,
 
 import "./Home.css"
 
-const family = [ {name: "spouse"}, {name: "child"}];
-
-const dietpreference = [{ pref: "Lactose Intolerant"}, {pref: "Vegan"}];
-
-const familyCard = (fMember) => {
-  return(
-  <ListGroupItem>
-    <Row>
-      <Col><img alt="UserEdit" src={Tphoto} className="userEdit"/></Col>
-      <Col> {fMember.name} </Col>
-      <Col/>
-      <Col/>
-      <Col> Edit</Col>
-    </Row>
-  </ListGroupItem>
-);
-}
-
-const dietCard = (pMember) => {
-  return(
-    <ListGroupItem>
-      <Row>
-        <p>{pMember.pref}</p>
-      </Row>
-    </ListGroupItem>
-);
-}
-
-const familyRender = family.map((fMember) => familyCard(fMember));
-
-const dietRender = dietpreference.map((pMember) => dietCard(pMember));
-
-
-
 export default class EditUser extends React.Component{
 
   constructor(props) {
     super(props);
     this.state = {
       prefModal: false,
+      userModal: false,
+      tempName: "",
+      family: [ {name: "spouse", id: 1}, {name: "child", id:2}],
+      preferences: [{ pref: "Lactose Intolerant" , id: 1}, {pref: "Vegan", id: 2}],
     };
     this.prefToggle = this.prefToggle.bind(this);
+    this.userToggle = this.userToggle.bind(this);
+    this.onChangename = this.onChangename.bind(this);
+    this.addFamily = this.addFamily.bind(this);
+    //this.remFam = this.remFamily.bind(this);
+    this.addPreference = this.addPreference.bind(this);
+    this.remPref = this.remPref.bind(this);
+  }
+
+  onChangename(e) {
+    this.setState({
+      tempName: e.target.value
+    });
+  }
+
+  addFamily(fMember) {
+    let key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    this.setState({ tempName: "", family: [...this.state.family, {name: fMember, id: key}]});
+  }
+
+  addPreference(fMember) {
+    let key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    this.setState({ tempName: "", preferences: [...this.state.preferences, {pref: fMember, id: key}]});
+  }
+
+   familyCard(fMember){
+    return(
+    <ListGroupItem style={{background: 'skyblue'}}>
+      <Row>
+        <Col><img alt="UserEdit" src={Bphoto} className="userEdit"/></Col>
+        <Col> {fMember.name} </Col>
+        <Col/>
+        <Col/>
+        <Button size="sm" onClick={() => this.remFamily(fMember.id)}>Remove</Button>
+      </Row>
+    </ListGroupItem>
+  );
+  }
+
+   dietCard(pMember) {
+    return(
+      <ListGroupItem style={{background: 'skyblue'}}>
+        <Row>
+          <Col><p>{pMember.pref}</p></Col>
+          <Col/>
+          <Button size="sm" onClick={() => this.remPref(pMember.id)}>Remove</Button>
+        </Row>
+      </ListGroupItem>
+  );
   }
 
   prefToggle() {
     const crntState = this.state.prefModal;
     this.setState({prefModal: !crntState}); }
+
+  userToggle() {
+    const crntState = this.state.userModal;
+    this.setState({userModal: !crntState}); }
+
+  remPref(key) {
+    let tempArr = this.state.preferences.filter(pref => pref.id !== key);
+    this.setState({preferences: tempArr});
+  }
+
+  remFamily(key) {
+    let tempArr = this.state.family.filter(pref => pref.id !== key);
+    this.setState({family: tempArr});
+  }
 
   logout() {
     //set usrname to null
@@ -64,28 +98,46 @@ export default class EditUser extends React.Component{
 
   render() {
     return (
-      <Container >
+      <div>
+      <Container style={{background: 'lightblue'}}>
+      <TopBar/>
 
       <Modal isOpen={this.state.prefModal}>
         <ModalHeader>Add Preference</ModalHeader>
         <ModalBody>
           <Label for="newPreference">Preference:</Label>
-          <Input type="preference" name="preference" id="newPreference" placeholder="Vegan, Lactose Intollerant, Immune to Fish, etc"/>
+          <Input type="preference" name="preference" id="newPreference" placeholder="Vegan, Lactose Intollerant, Immune to Fish, etc"
+          value={this.state.tempName}
+          onChange={this.onChangename}/>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={this.prefToggle}> Add </Button> {' '}
+          <Button color="primary" onClick={() => {this.prefToggle(); this.addPreference(this.state.tempName);}}> Add </Button> {' '}
           <Button color="secondary" onClick={this.prefToggle}> Cancel </Button>
         </ModalFooter>
       </Modal>
 
-        <ListGroup className="profile">
+      <Modal isOpen={this.state.userModal}>
+        <ModalHeader>Add Family</ModalHeader>
+        <ModalBody>
+          <Label for="newPreference">Family Member Name: </Label>
+          <Input type="user" name="user" id="newUser" placeholder="Mary Jane, Peter Parker, etc"
+          value={this.state.tempName}
+          onChange={this.onChangename}/>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => {this.userToggle(); this.addFamily(this.state.tempName);}}> Add </Button> {' '}
+          <Button color="secondary" onClick={this.userToggle}> Cancel </Button>
+        </ModalFooter>
+      </Modal>
+
+        <ListGroup className="profile" >
               <ListGroupItem >
                 <Row>
-                  <Col><NavLink href="/Home/:id/:password"> + </NavLink> </Col>
+                  <Col ><NavLink href="/Home/:id/:password"> + </NavLink> </Col>
                   <Col><h3>Profile</h3></Col><Col/><Col/><Col/>
                 </Row>
               </ListGroupItem>
-              <ListGroupItem>
+              <ListGroupItem style={{background: 'lightblue'}}>
                 <Row>
                   <h4> Guest Name </h4>
                   <Col></Col>
@@ -94,26 +146,25 @@ export default class EditUser extends React.Component{
                 </Row>
               </ListGroupItem>
 
-              <ListGroupItem>
+              <ListGroupItem style={{background: 'lightblue'}}>
                 <Row>
                   <h5> Family </h5>
                   <Col/>
                   <Col/>
                   <Col/>
-                  <Col xs='1'> + </Col>
+                  <Button size="sm" onClick={this.userToggle}> + </Button>
                 </Row>
               </ListGroupItem>
 
-              {familyRender}
+              {this.state.family.map((pMember) => this.familyCard(pMember))}
 
-
-              <ListGroupItem>
+              <ListGroupItem style={{background: 'lightblue'}}>
                 <Row>
                   <p> Add more people to your shared fridge, shopping list and recipies</p>
                 </Row>
               </ListGroupItem>
 
-              <ListGroupItem>
+              <ListGroupItem style={{background: 'lightblue'}}>
                 <Row>
                   <h5> Diet Preferences </h5>
                   <Col/>
@@ -123,9 +174,9 @@ export default class EditUser extends React.Component{
                 </Row>
               </ListGroupItem>
 
-              {dietRender}
+              {this.state.preferences.map((pMember) => this.dietCard(pMember))}
 
-              <ListGroupItem >
+              <ListGroupItem style={{background: 'lightblue'}}>
                 <Row>
                   <h5> Notifications </h5>
                   <Col/>
@@ -134,7 +185,7 @@ export default class EditUser extends React.Component{
                   <Col xs='1'> > </Col>
                 </Row>
               </ListGroupItem>
-              <ListGroupItem><NavLink href="/about">
+              <ListGroupItem style={{background: 'lightblue'}}><NavLink style={{color: "black"}} href="/about">
                 <Row>
                   <h5> About Us </h5>
                   <Col/>
@@ -143,16 +194,7 @@ export default class EditUser extends React.Component{
                   <Col xs='1'> > </Col>
                 </Row> </NavLink>
               </ListGroupItem>
-              <ListGroupItem >
-                <Row>
-                  <h5> Settings </h5>
-                  <Col/>
-                  <Col/>
-                  <Col/>
-                  <Col xs='1'> > </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem>
+              <ListGroupItem style={{background: 'lightblue'}}>
                 <Row>
                   <Col>
                     <Button href="/" onClick={this.logout} size="lg">Log Out</Button>
@@ -163,7 +205,7 @@ export default class EditUser extends React.Component{
 
 
       </Container>
-
+      </div>
     );
   }
 }
